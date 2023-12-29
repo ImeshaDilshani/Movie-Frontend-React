a9d4abdafbaf86b9708a1fe4b532bd6f5a8fecf9
ImeshaDilshani <imeshadilshani212@gmail.com>
2023-12-29 20:11:37 +0530

    cross origin enable

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export default axios.create({
     
-    baseURL: "http://localhost:8080/api/v1/movies",
+    baseURL: "http://localhost:8080",
     headers: {"ngrok-skip-browser-warning": "true"}
 });
